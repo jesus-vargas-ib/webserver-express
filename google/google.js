3262b5ae2c8a25dcd44a67b7ec01ce18a8d570fb
Jesus Vargas <jesus.vargas@codes.com.mx>
2018-12-07 23:03:30 -0600

    Se cambia forma de cargar archivo JSON

--- google/google.js
+++ google/google.js
@@ -2,9 +2,16 @@
 const { Storage } = require('@google-cloud/storage');
 const projectId = 'bco-codes';
 const CLOUD_BUCKET = 'ejemplo-node';
+
+const fs = require('fs');
+const path = require('path');
+const gTokenPath = path.join(`${__dirname}/gToken.json`);
+fs.writeFileSync(gTokenPath, process.env.GCS_JSON_TOKEN);
+const gcsKeyFile = JSON.parse(process.env.GCS_JSON_TOKEN);
+
 const storage = new Storage({
     projectId: projectId,
-    keyFilename: './google/config/bco-codes-869c8359ed95.json'
+    keyFilename: gTokenPath,
 });
 const bucket = storage.bucket(CLOUD_BUCKET);
 
